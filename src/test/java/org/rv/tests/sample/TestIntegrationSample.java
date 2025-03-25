363a6dfb1f33f14aacd0b198c389bd3750ffe688
ritwik vinay <ritwikvinay38@gmail.com>
2025-03-25 09:53:24 +0530

    Initial commit 1

--- src/test/java/org/rv/tests/sample/TestIntegrationSample.java
+++ src/test/java/org/rv/tests/sample/TestIntegrationSample.java
@@ -1,4 +1,16 @@
 package org.rv.tests.sample;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Owner;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
 public class TestIntegrationSample {
+    @Test(groups="qa", priority=1)
+    @Owner("Ritwik")
+    @Description("TC# TC1 = Step1 verify that the booking can be created")
+    public void testcreateBooking(){
+        Assert.assertTrue(true);
+
+    }
 }
